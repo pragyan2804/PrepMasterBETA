30c58279d81ae2e6bd152a879f3c03f8dc2f0b0e
Pragyan Sharma <99196461+pragyan2804@users.noreply.github.com>
2022-03-05 14:10:45 +0530

    Delete PreReq directory

--- PreReq/installer.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-echo current path - %cd%
-echo INITIALIZING INSTALLER....
-cd "C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python39\Scripts"
-echo installation path -  %cd%
-start pip install Pillow
-start pip install mysql-connector-python
-echo DONE!
-PAUSE
